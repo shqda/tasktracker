1e635def18d582a2f4a2edf3406b18362bab6e1b
Artyom Nikitin <artemf642@gmail.com>
2026-01-08 21:12:48 +0300

    main router params changed to default

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"TaskTracker_/internal/server"
-	"TaskTracker_/internal/server/handlers"
-	"TaskTracker_/internal/services"
-	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func main() {
-	r := server.NewRouter(gin.Default(), handlers.NewTaskHandler(services.NewTaskService()))
+	r := server.NewRouter(nil, nil)
 	r.RegisterRoutes()
 	err := r.Engine.Run(":8080")
 	if err != nil {
