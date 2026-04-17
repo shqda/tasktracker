4b3d5103ad2b0086b90665b13d8d290629c14a30
shqda <artemf642@gmail.com>
2026-04-17 15:54:00 +0300

    feat: add graceful shutdown

--- cmd/task_tracker/main.go
+++ cmd/task_tracker/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
 	"tasktracker/config"
 	"tasktracker/internal/server"
 	"tasktracker/internal/server/handler"
 	"tasktracker/internal/service"
 	"tasktracker/internal/storage/postgres"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -26,11 +31,16 @@ func main() {
 		cfg.DB.Name,
 		"disable",
 	)
-
 	db, err := sqlx.Connect("postgres", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer func() {
+		err = db.Close()
+		if err != nil {
+			log.Printf("error closing db: %v", err)
+		}
+	}()
 
 	repo := &postgres.PostgresDB{DB: db}
 	svc := service.NewTaskService(repo)
@@ -40,7 +50,27 @@ func main() {
 	r.RegisterRoutes()
 
 	addr := fmt.Sprintf(":%s", cfg.Serv.Port)
-	if err := r.Engine.Run(addr); err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r.Engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Stopped listening: %v\n", err)
+			log.Fatal(err)
+		}
+	}()
+
+	shutdown, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+	<-shutdown.Done()
+
+	fmt.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Printf("Shutdown with error: %v", err)
 	}
 }
