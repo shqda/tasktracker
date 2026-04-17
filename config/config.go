64e02facc7b41c5fc6b4bb78883fa8512c679765
shqda <artemf642@gmail.com>
2026-04-17 14:48:40 +0300

    add config files

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type Config struct {
+	Serv ConfigServer   `yaml:"server"`
+	DB   ConfigDatabase `yaml:"db"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	cfg := &Config{}
+	if err := cleanenv.ReadConfig(path, cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+type ConfigServer struct {
+	Port string `yaml:"port" env:"SERVER_PORT" env-default:"8080"`
+}
+
+type ConfigDatabase struct {
+	Host     string `yaml:"host" env:"DB_HOST" env-default:"localhost"`
+	Port     string `yaml:"port" env:"DB_PORT" env-default:"5432"`
+	User     string `yaml:"user" env:"DB_USER" env-default:"postgres"`
+	Password string `yaml:"password" env:"DB_PASSWORD" env-default:"postgres"`
+	Name     string `yaml:"name" env:"DB_NAME" env-default:"tasktracker"`
+}
